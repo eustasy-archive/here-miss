b9c924aba6e0bfb57632d8032facb1c6707a0f44
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-11-22 20:47:45 +0000

    Improve JS

--- here-miss.js
+++ here-miss.js
@@ -1,3 +1,27 @@
+////	Here, Miss.
+// An DoNotTrack detection script that ignores settings done automatically.
+// Copyright Lewis Goddard 2014 - MIT Licensed
+//
+////	Inputs
+//
+// respectIE = bool(false)
+// A boolean value on whether to respect later IE versions that turn on DoNotTrack by default.
+// Defaults to `false`
+//
+////	Outputs
+//
+// iev = int(11)
+// The Internet Explorer version. An integer if Internet Explorer, `false` if not.
+//
+// isie = bool(true)
+// A boolean value to check if this is Internet Explorer.
+//
+// dnt = bool(false)
+// A boolean value that states whether the DoNotTrack header was set.
+//
+// trackme = bool(true)
+// A boolean value that states whether the user allows tracking.
+
 var iev = false;
 var isie = false;
 var dnt = false;
@@ -6,22 +30,19 @@ if ( typeof respectIE === 'undefined' ) {
 	respectIE = false;
 }
 
+// 5 - 10
+var msie = ua.indexOf('MSIE ');
+if ( msie > -1 ) {
+	iev = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+
 // >= 11 (Majority)
-if ( ua.indexOf('Trident/') > -1 ) {
+} else if ( ua.indexOf('Trident/') > -1 ) {
 	var rv = ua.indexOf('rv:');
 	iev = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-} else {
-
-	// 5 - 10
-	var msie = ua.indexOf('MSIE ');
-	if ( msie > -1 ) {
-		iev = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-	}
-
-	// No detection of 0 - 4
-	// Rarely used.
-
 }
+
+// No detection of 0 - 4
+// Rarely used.
 
 // If is IE, mark it as so.
 if ( iev ) {
